648768a4ce42b964577cc56958094d73872d52f1
Julian Storer <julianstorer@gmail.com>
2021-12-03 10:39:34 +0000

    Moved the unit test helper classes into a separate file, so they can be used without pulling in all the choc tests

--- tests/choc_tests.h
+++ tests/choc_tests.h
@@ -54,7 +54,7 @@
 #include "../audio/choc_AudioMIDIBlockDispatcher.h"
 #include "../javascript/choc_javascript.h"
 
-#include <iostream>
+#include "choc_UnitTest.h"
 
 /**
     To keep things simpole for users, I've just shoved all the tests for everything into this
@@ -70,146 +70,12 @@
 namespace choc::test
 {
 
-/// Keeps track of the number of passes/fails for a test-run.
-struct TestProgress
-{
-    void startCategory (std::string category);
-    void startTest (std::string_view testName);
-    void endTest();
-    void fail (const char* filename, int lineNumber, std::string_view message);
-    void check (bool, const char* filename, int lineNumber, std::string_view failureMessage);
-    void print (std::string_view);
-
-    /// Call this at the end to print out the number of passes and fails
-    void printReport();
-
-    /// This is used to print progress messages as the tests run. If you don't
-    /// supply your own funciton here, the default will print to std::cout
-    std::function<void(std::string_view)> printMessage;
-
-    std::string currentCategory, currentTest;
-    int numPasses = 0, numFails = 0;
-    bool currentTestFailed = false;
-    std::vector<std::string> failedTests;
-};
-
 /// Just create a TestProgress and pass it to this function to run all the
 /// tests. The TestProgress object contains a callback that will be used
 /// to log its progress.
 bool runAllTests (TestProgress&);
 
 
-// Some macros to use to perform tests. If you want to use this to write your own tests,
-// have a look at the tests for choc functions, and it should be pretty obvious how the
-// macros are supposed to be used.
-
-#define CHOC_CATEGORY(category)          progress.startCategory (#category);
-#define CHOC_TEST(name)                  choc::test::ScopedTest scopedTest_ ## __LINE__ (progress, #name);
-#define CHOC_FAIL(message)               progress.fail (__FILE__, __LINE__, message);
-#define CHOC_EXPECT_TRUE(b)              progress.check (b, __FILE__, __LINE__, "Expected " #b);
-#define CHOC_EXPECT_FALSE(b)             progress.check (! (b), __FILE__, __LINE__, "Expected ! " #b);
-#define CHOC_EXPECT_EQ(a, b)             { auto x = a; auto y = b; progress.check (x == y, __FILE__, __LINE__, "Expected " #a " (" + choc::test::convertToString (x) + ") == " #b " (" + choc::test::convertToString (y) + ")"); }
-#define CHOC_EXPECT_NE(a, b)             { auto x = a; auto y = b; progress.check (x != y, __FILE__, __LINE__, "Expected " #a " (" + choc::test::convertToString (x) + ") != " #b); }
-#define CHOC_EXPECT_NEAR(a, b, diff)     { auto x = a; auto y = b; auto d = diff; progress.check (std::abs (x - y) <= d, __FILE__, __LINE__, #a " (" + choc::test::convertToString (x) + ") and " #b " (" + choc::test::convertToString (y) + ") differ by more than " + choc::test::convertToString (d)); }
-#define CHOC_CATCH_UNEXPECTED_EXCEPTION  catch (...) { CHOC_FAIL ("Unexpected exception thrown"); }
-
-
-//==============================================================================
-//        _        _           _  _
-//     __| |  ___ | |_   __ _ (_)| | ___
-//    / _` | / _ \| __| / _` || || |/ __|
-//   | (_| ||  __/| |_ | (_| || || |\__ \ _  _  _
-//    \__,_| \___| \__| \__,_||_||_||___/(_)(_)(_)
-//
-//   Code beyond this point is implementation detail...
-//
-//==============================================================================
-
-inline void TestProgress::print (std::string_view message)
-{
-    if (printMessage != nullptr)
-        printMessage (message);
-    else
-        std::cout << message << std::endl;
-}
-
-inline void TestProgress::startCategory (std::string category)
-{
-    currentCategory = std::move( category);
-}
-
-inline void TestProgress::startTest (std::string_view testName)
-{
-    CHOC_ASSERT (! currentCategory.empty());
-    currentTest = currentCategory + "/" + std::string (testName);
-    currentTestFailed = false;
-    print ("[ RUN      ] " + currentTest);
-}
-
-inline void TestProgress::endTest()
-{
-    if (currentTestFailed)
-    {
-        print ("[     FAIL ] " + currentTest);
-        ++numFails;
-        failedTests.push_back (currentTest);
-    }
-    else
-    {
-        print ("[       OK ] " + currentTest);
-        ++numPasses;
-    }
-
-    currentTest = {};
-}
-
-inline void TestProgress::fail (const char* filename, int lineNumber, std::string_view message)
-{
-    currentTestFailed = true;
-    CHOC_ASSERT (! currentTest.empty());
-    print ("FAILED: " + std::string (filename) + ":" + std::to_string (lineNumber));
-    print (message);
-}
-
-inline void TestProgress::check (bool condition, const char* filename, int lineNumber, std::string_view message)
-{
-    if (! condition)
-        fail (filename, lineNumber, message);
-}
-
-inline void TestProgress::printReport()
-{
-    print ("========================================================");
-    print (" Passed:      " + std::to_string (numPasses));
-    print (" Failed:      " + std::to_string (numFails));
-
-    for (auto& failed : failedTests)
-        print ("  Failed test: " + failed);
-
-    print ("========================================================");
-}
-
-struct ScopedTest
-{
-    ScopedTest (TestProgress& p, std::string name) : progress (p)   { progress.startTest (std::move (name)); }
-    ~ScopedTest()                                                   { progress.endTest(); }
-
-    TestProgress& progress;
-};
-
-template <typename Type>
-std::string convertToString (Type n)
-{
-    if constexpr (std::is_same<const Type, const char* const>::value)           return std::string (n);
-    else if constexpr (std::is_same<const Type, const std::string>::value)      return n;
-    else if constexpr (std::is_same<const Type, const std::string_view>::value) return std::string (n);
-    else                                                                        return std::to_string (n);
-}
-
-//==============================================================================
-//
-//  The tests themselves....
-//
 //==============================================================================
 inline void testContainerUtils (TestProgress& progress)
 {
