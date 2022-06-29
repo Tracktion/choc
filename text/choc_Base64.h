1f9b5dafc6e26ec14ae916b1ee6c970f474290f7
Julian Storer <julianstorer@gmail.com>
2022-06-29 18:35:01 +0100

    Added a cast to Base64

--- text/choc_Base64.h
+++ text/choc_Base64.h
@@ -196,7 +196,7 @@ template <typename OutputContainer>
 bool decodeToContainer (OutputContainer& outputContainer, std::string_view base64)
 {
     outputContainer.reserve ((base64.length() / 4u) * 3u + 4u);
-    return decode (base64, [&] (uint8_t b) { outputContainer.push_back (b); });
+    return decode (base64, [&] (uint8_t b) { outputContainer.push_back (static_cast<typename OutputContainer::value_type> (b)); });
 }
 
 
