62aaaa8e217a682aee7f84e7a5dc9df96737b89f
Anthony Nicholls <anthony@juce.com>
2024-10-30 16:43:02 +0000

    Fix arm64ec compilation issue

--- math/choc_MathHelpers.h
+++ math/choc_MathHelpers.h
@@ -30,7 +30,7 @@
   #pragma intrinsic (_BitScanReverse64)
  #endif
 
- #ifdef _M_X64
+ #if defined (_M_X64) && ! defined (_M_ARM64EC)
   #pragma intrinsic (_umul128)
   #define CHOC_HAS_UMUL128 1
  #endif
