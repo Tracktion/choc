f0f5cdf5a938b8b779fea6c083571cce5ccab925
Julian Storer <julianstorer@gmail.com>
2026-03-29 09:56:43 +0100

    Fix for getBuildDate() to cope with timezones

--- choc/platform/choc_BuildDate.h
+++ choc/platform/choc_BuildDate.h
@@ -20,45 +20,50 @@
 #define CHOC_BUILDDATE_HEADER_INCLUDED
 
 #include <chrono>
+#include <ctime>
 
 namespace choc
 {
     /// Returns the date and time at which this function was compiled, by
     /// parsing the __DATE__ and __TIME__ macros into a std::chrono timepoint.
-    constexpr std::chrono::system_clock::time_point getBuildDate()
+    /// The compiler macros provide local time, so std::mktime is used to
+    /// correctly convert to UTC.
+    inline std::chrono::system_clock::time_point getBuildDate()
     {
-        using namespace std::chrono;
+        constexpr int mon = __DATE__[0] == 'J' ? (__DATE__[1] == 'a' ? 0
+                                                : (__DATE__[2] == 'n' ? 5 : 6))
+                          : __DATE__[0] == 'F' ? 1
+                          : __DATE__[0] == 'M' ? (__DATE__[2] == 'r' ? 2 : 4)
+                          : __DATE__[0] == 'A' ? (__DATE__[1] == 'p' ? 3 : 7)
+                          : __DATE__[0] == 'S' ? 8
+                          : __DATE__[0] == 'O' ? 9
+                          : __DATE__[0] == 'N' ? 10
+                          : __DATE__[0] == 'D' ? 11
+                          : 0;
 
-        constexpr month m (__DATE__[0] == 'J' ? (__DATE__[1] == 'a' ? 1u
-                                              : (__DATE__[2] == 'n' ? 6u : 7u))
-                         : __DATE__[0] == 'F' ? 2
-                         : __DATE__[0] == 'M' ? (__DATE__[2] == 'r' ? 3u : 5u)
-                         : __DATE__[0] == 'A' ? (__DATE__[1] == 'p' ? 4u : 8u)
-                         : __DATE__[0] == 'S' ? 9u
-                         : __DATE__[0] == 'O' ? 10u
-                         : __DATE__[0] == 'N' ? 11u
-                         : __DATE__[0] == 'D' ? 12u
-                         : 0u);
+        constexpr int day = (__DATE__[4] == ' ') ? (__DATE__[5] - '0')
+                                                 : (__DATE__[4] - '0') * 10
+                                                     + (__DATE__[5] - '0');
 
-        constexpr day d ((__DATE__[4] == ' ') ? static_cast<unsigned> (__DATE__[5] - '0')
-                                              : static_cast<unsigned> ((__DATE__[4] - '0') * 10
-                                                                         + (__DATE__[5] - '0')));
+        constexpr int year = (__DATE__[7] - '0') * 1000
+                               + (__DATE__[8] - '0') * 100
+                               + (__DATE__[9] - '0') * 10
+                               + (__DATE__[10] - '0');
 
-        constexpr year y ((__DATE__[7] - '0') * 1000
-                            + (__DATE__[8] - '0') * 100
-                            + (__DATE__[9] - '0') * 10
-                            + (__DATE__[10] - '0'));
+        constexpr int hrs = (__TIME__[0] - '0') * 10 + (__TIME__[1] - '0');
+        constexpr int min = (__TIME__[3] - '0') * 10 + (__TIME__[4] - '0');
+        constexpr int sec = (__TIME__[6] - '0') * 10 + (__TIME__[7] - '0');
 
-        constexpr auto hrs = static_cast<unsigned> ((__TIME__[0] - '0') * 10 + (__TIME__[1] - '0'));
-        constexpr auto min = static_cast<unsigned> ((__TIME__[3] - '0') * 10 + (__TIME__[4] - '0'));
-        constexpr auto sec = static_cast<unsigned> ((__TIME__[6] - '0') * 10 + (__TIME__[7] - '0'));
+        std::tm tm = {};
+        tm.tm_year = year - 1900;
+        tm.tm_mon  = mon;
+        tm.tm_mday = day;
+        tm.tm_hour = hrs;
+        tm.tm_min  = min;
+        tm.tm_sec  = sec;
+        tm.tm_isdst = -1;
 
-        constexpr auto count = sys_days (year_month_day (y, m, d))
-                                + hours (hrs)
-                                + minutes (min)
-                                + seconds (sec);
-
-        return time_point<system_clock, seconds> (count);
+        return std::chrono::system_clock::from_time_t (std::mktime (&tm));
     }
 
     /// Returns the number of whole days that have elapsed since this
